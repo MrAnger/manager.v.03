bf95939912f21bf34f4de394f36d859185dfd877
StingStyle <skv.max@mail.ru>
2013-04-12 05:33:53 +0400

    add

--- js/main.js
+++ js/main.js
@@ -35,92 +35,6 @@ $(".show-box").disableSelection().click(function (event) {
     }
     event.stopImmediatePropagation();
 });
-<<<<<<< HEAD
-//  авторизация/регистрация/восстановление
-signList = function (event) {
-    $("[name=confirm-box]").hide();
-    $('.center-box form').hide();
-    var item = $(this).attr('name');
-    switch (item) {
-        case 'reg':
-            $('form[name=reg]').fadeIn(550).slideDown(500);
-            break;
-        case 'auth':
-            $('form[name=auth]').fadeIn(550).slideDown(500);
-            break;
-        case 'forgot':
-            $('form[name=forgot]').fadeIn(550).slideDown(500);
-            break;
-    }
-    event.stopImmediatePropagation();
-};
-//  функции при загрузке документа
-$(document).ready(function () {
-    $(".link").on("click", signList);
-    $(".forgot-cancel").on("click", function () {
-        $('.center-box form').hide();
-        $('form[name=auth]').fadeIn(550).slideDown(500);
-    });
-    /*$("form[name=auth]").submit(function () {
-        $('.center-box').hide();
-        $('.center-box form').hide();
-        $('.manager').fadeIn(550).slideDown(500);
-        $('.login-content').fadeIn(550).slideDown(500);
-    });*/
-    $("form[name=forgot]").submit(function () {
-        $("[name=confirm-box]").fadeIn(550).slideDown(500);
-    });
-});
-//  скроллинг
-function setHeightContentScroll() {
-    $('.content-scroll').css("height", $(window).height() - 30);
-    $('.manager .content-scroll').css("height", $(window).height() - 60);
-    $('.content-scroll').children().css("top", 0);
-};
-setHeightContentScroll();
-$(window).resize(setHeightContentScroll);
-$('.center-box, .content-idea.one .item-list, .content-idea.two .item-list, .content-idea.three .three-content').mousewheel(function (e, delta) {
-    var scrollSize = 10;
-    if (delta > 0) {
-        if (parseInt($(this).css("top")) != 0) {
-            $(this).css("top", parseInt($(this).css("top")) + scrollSize * delta);
-        }
-    } else {
-        if ($(this).parent().height() + Math.abs(parseInt($(this).css("top"))) <= $(this).height()) {
-            $(this).css("top", parseInt($(this).css("top")) + scrollSize * delta);
-        }
-    }
-});
-//  языки
-$('.lng .show').click(function () {
-    $(this).toggleClass('true');
-    if ($(this).hasClass('true')) {
-        $('.lng-check').fadeIn(10).slideDown(200);
-    }
-    else {
-        $('.lng-check').fadeOut(10).slideOut(200);
-    }
-});
-$('.lng-check li').click(function () {
-    $('.lng-check .lng-item').removeClass('active');
-    $(this).children('span').addClass('active');
-    $('.lng .show').removeClass('true');
-    $('.lng-check').fadeOut(10).slideOut(200);
-});
-//  мессаги
-$('.msg-box .close').live("click", function(e){
-	$(this).parent().fadeOut(300).slideUp(200);
-});
-//  лоадер
-function loader() {
-    $('.loader').fadeOut(300).slideUp(200);
-};
-$('.loader').on("ready", function () {
-    setTimeout("loader();", 1000);
-});
-//  менеджер/списки
-=======
->>>>>>> add
 $('.item-list li').click(function (event) {
     $(this).parent().children("li").removeClass('active');
     $(this).addClass('active');
@@ -153,12 +67,6 @@ $('.tab-list li').click(function () {
 $('.edit').click(function () {
     $(this).toggleClass('active');
 });
-<<<<<<< HEAD
-$(".context-list .list").on("click", function (event) {
-    $(this).parents(".context-option").removeClass("active");
-    event.stopImmediatePropagation();
-});
-=======
 //  скроллинг
 function setHeightContentScroll() {
     $('.setting-group').css("height", $(window).height() - 90);
@@ -190,4 +98,3 @@ $(".spoiler").click(function () {
 $(".loader").click(function(){
     $(this).hide()
 })
->>>>>>> add
